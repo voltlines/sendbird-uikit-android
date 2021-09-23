b24337ba9350a950865de86caf526b99af63d0bf
Mustafa Süleyman Kınık <suleymankinik@outlook.com>
2021-09-23 17:57:45 +0300

    Updated string of no channel Added new icon for no channel

--- uikit/src/main/java/com/sendbird/uikit/fragments/ChannelFragment.java
+++ uikit/src/main/java/com/sendbird/uikit/fragments/ChannelFragment.java
@@ -433,7 +433,7 @@ public class ChannelFragment extends BaseGroupChannelFragment implements OnItemC
         });
 
         if (args != null && args.containsKey(StringSet.KEY_EMPTY_ICON_RES_ID)) {
-            int emptyIconResId = args.getInt(StringSet.KEY_EMPTY_ICON_RES_ID, R.drawable.icon_chat);
+            int emptyIconResId = args.getInt(StringSet.KEY_EMPTY_ICON_RES_ID, R.drawable.ic_no_message);
             binding.statusFrame.setEmptyIcon(emptyIconResId);
             ColorStateList emptyIconTint = args.getParcelable(StringSet.KEY_EMPTY_ICON_TINT);
             binding.statusFrame.setIconTint(emptyIconTint);
